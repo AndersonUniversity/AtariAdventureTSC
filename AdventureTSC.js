631b309b47d496209f0ea3db11ec373039ea4d5e
Tyler <tscooper97@gmail.com>
2018-09-20 20:47:56 -0400

    added border so player can not move off screen

--- AdventureTSC.js
+++ AdventureTSC.js
@@ -1,21 +1,38 @@
 let myGamePiece;
 
+const screenHeight = 576;
+const screenWidth = 1024;
+const pieceHeight = 70;
+const pieceWidth = 30;
+const leftArrow = 37;
+const rightArrow = 39;
+const upArrow = 38;
+const downArrow = 40;
+const movementSpeed = 1;
+const stoppedSpeed = 0;
+const leftWall = 0;
+const topWall = 0;
+const startX = 50;
+const startY = 250;
+const interval = 20;
+
+
 function startGame() {
     myGameArea.start();
-    myGamePiece = new Component(30, 70, "blue", 50, 50);
+    myGamePiece = new Component(pieceWidth, pieceHeight, "blue", startX, startY);
 }
 
 //define the canvas
 let myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        //setting dimenstions of canvas
-        this.canvas.width = 1024;
-        this.canvas.height = 576;
+        //setting dimensions of canvas
+        this.canvas.width = screenWidth;
+        this.canvas.height = screenHeight;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         //how many times per second to redraw the screen
-        this.interval = setInterval(updateGameArea, 20);
+        this.interval = setInterval(updateGameArea, interval);
         //listening to keyboard
         window.addEventListener('keydown', function(e) {myGameArea.key = e.keyCode;})
         window.addEventListener('keyup', function(e) {myGameArea.key = false;})
@@ -30,8 +47,8 @@ let myGameArea = {
 function Component(width, height, color, x, y) {
     this.width = width;
     this.height = height;
-    this.speedX = 0;
-    this.speedY = 0;
+    this.speedX = stoppedSpeed;
+    this.speedY = stoppedSpeed;
     this.x = x;
     this.y = y;
     //how to update component attributes
@@ -57,16 +74,16 @@ function updateGameArea() {
 
 //moving the player based on the arrow key that is pressed
 function decideMove() {
-    if(myGameArea.key && myGameArea.key === 37) {
+    if(myGameArea.key && myGameArea.key === leftArrow) {
         moveLeft();
     }
-    else if(myGameArea.key && myGameArea.key === 39) {
+    else if(myGameArea.key && myGameArea.key === rightArrow) {
         moveRight();
     }
-    else if(myGameArea.key && myGameArea.key === 38) {
+    else if(myGameArea.key && myGameArea.key === upArrow) {
         moveUp();
     }
-    else if(myGameArea.key && myGameArea.key === 40) {
+    else if(myGameArea.key && myGameArea.key === downArrow) {
         moveDown();
     }
     else {
@@ -76,22 +93,46 @@ function decideMove() {
 
 //functions that dictate player movement
 function moveUp() {
-    myGamePiece.speedY -= 1;
+    myGamePiece.speedY -= movementSpeed;
+    restrictPlayer();
 }
 
 function moveDown() {
-    myGamePiece.speedY += 1;
+    myGamePiece.speedY += movementSpeed;
+    restrictPlayer();
 }
 
 function moveLeft() {
-    myGamePiece.speedX -= 1;
+    myGamePiece.speedX -= movementSpeed;
+    restrictPlayer();
 }
 
 function moveRight() {
-    myGamePiece.speedX += 1;
+    myGamePiece.speedX += movementSpeed;
+    restrictPlayer();
 }
 
 function stopMovement() {
-    myGamePiece.speedX = 0;
-    myGamePiece.speedY = 0;
+    myGamePiece.speedX = stoppedSpeed;
+    myGamePiece.speedY = stoppedSpeed;
+}
+
+//make sure player does not move off the screen
+function restrictPlayer() {
+    if(myGamePiece.x < leftWall) {
+        stopMovement();
+        myGamePiece.x = leftWall;
+    }
+    if(myGamePiece.y < topWall) {
+        stopMovement();
+        myGamePiece.y = topWall;
+    }
+    if(myGamePiece.x > myGameArea.canvas.width - pieceWidth) {
+        stopMovement();
+        myGamePiece.x = myGameArea.canvas.width - pieceWidth;
+    }
+    if(myGamePiece.y > myGameArea.canvas.height - pieceHeight) {
+        stopMovement();
+        myGamePiece.y = myGameArea.canvas.height - pieceHeight;
+    }
 }
